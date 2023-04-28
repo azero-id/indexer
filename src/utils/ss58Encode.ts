93bff5987991dcf1ae1fba8879085ab04470ad7d
Dennis Zoma <dennis@azero.domains>
2023-04-28 07:55:45 +0200

    fix: Chain prefix type

--- src/utils/ss58Encode.ts
+++ src/utils/ss58Encode.ts
@@ -4,7 +4,7 @@ import * as ss58 from '@subsquid/ss58'
  * Helper to encode an address to SS58 format with the defined chain prefix.
  */
 export const ss58Encode = (address: Uint8Array | undefined): string | undefined => {
-  const prefix = process.env.CHAIN_PREFIX
+  const prefix = parseInt(process.env.CHAIN_PREFIX || '')
   if (!prefix) {
     throw new Error('`CHAIN_PREFIX` environment variable is not set.')
   }
