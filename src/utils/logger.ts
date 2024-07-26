d3dbc16e5a5d5399060cdad88f8cb73d4f3414fc
Dennis Zoma <dennis@azero.id>
2024-07-26 08:27:53 +0200

    fix: Pino to display objects provided as second arguments

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,17 @@
 import pino from 'pino'
 
 export const logger = pino({
+  hooks: {
+    // Pino expects object to be the first argument
+    logMethod(inputArgs, method) {
+      if (inputArgs.length >= 2) {
+        const arg1 = inputArgs.shift()
+        const arg2 = inputArgs.shift()
+        return method.apply(this, [arg2, arg1, ...inputArgs])
+      }
+      return method.apply(this, inputArgs)
+    },
+  },
   level: process.env.LOG_LEVEL || 'info',
   transport: {
     target: 'pino-pretty',
